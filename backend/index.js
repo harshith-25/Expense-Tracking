4288b788d6ddddd5df595b8872d75544e1b02797
Harshith <harshithsshetty007@gmail.com>
2024-09-05 08:40:27 +0530

    Expense Tracking

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require('./Models/db');
+const AuthRouter = require('./Routes/AuthRouter');
+const ProductRouter = require('./Routes/ProductRouter');
+const ExpenseRouter = require('./Routes/ExpenseRouter');
+const ensureAuthenticated = require("./Middlewares/Auth");
+const PORT = process.env.PORT || 8080;
+
+app.get('/ping', (req,res) => {
+	res.send("pong");
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+app.use('/expenses',ensureAuthenticated, ExpenseRouter);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on PORT - ${PORT}`);
+})
